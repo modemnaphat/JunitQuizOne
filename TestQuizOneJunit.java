d1c1399a36abcb4700902a52ba81c72619ac359a
modemnaphat <ModemNaphat@gmail.com>
2023-12-18 11:12:03 +0700

    add

---
+++ TestQuizOneJunit.java
@@ -0,0 +1,30 @@
+public class TestQuizOneJunit {
+    QuizOneJunit obj = new QuizOneJunit();
+    
+    //1
+    @Test
+    public void tc11(){
+    
+    }
+    
+    //2
+    @Test
+    public void tc21(){
+            
+    }
+    
+    @Test
+    public void tc22(){
+            
+    }
+    
+    //3
+    @Test
+    public void tc31(){
+            
+    }
+    @Test
+    public void tc32(){
+            
+    }
+}
